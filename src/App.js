a6ac9719a3df86bca484b9bcd278f54b04b5f1f1
Sneha2622001 <mesnehasingh26@gmail.com>
2023-01-30 23:18:10 +0530

    loader added

--- src/App.js
+++ src/App.js
@@ -7,33 +7,46 @@ import News from "./components/News";
 
 //copeid from react router dom website
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar' 
 
 export default class App extends Component {
+  pageSize = 5;
+
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) => {
+    this.setState({progress: progress})
+  }
   render() {
     return (
       <div>
         <BrowserRouter>
           <NavBar />
-
+          <LoadingBar
+            color='#f11946'
+            progress={this.state.progress}
+          />
           <Routes>
-            <Route exact path="/" element={<News key="general" pageSize={6} country="in" category="general" />}>   </Route>
+            <Route exact path="/" element={<News setProgress={this.setProgress} key="general" pageSize={6} country="in" category="general" />}>   </Route>
 
-            <Route exact path="/general" element={<News key="general" pageSize={6} country="in" category="general" />}>   </Route>
+            <Route exact path="/general" element={<News setProgress={this.setProgress} key="general" pageSize={6} country="in" category="general" />}>   </Route>
               
          
-            <Route exact path="/business" element={<News key="business" pageSize={6} country="in" category="business" />}></Route>
+            <Route exact path="/business" element={<News setProgress={this.setProgress} key="business" pageSize={6} country="in" category="business" />}></Route>
               
             
-            <Route exact path="/entertainment" element={ <News key="entertainment" pageSize={6} country="in" category="entertainment" />}></Route>
+            <Route exact path="/entertainment" element={ <News setProgress={this.setProgress} key="entertainment" pageSize={6} country="in" category="entertainment" />}></Route>
             
 
-            <Route exact path="/health" element={<News key="health" pageSize={6} country="in" category="health" />}> </Route>
-            <Route exact path="/science" element={ <News key="science" pageSize={6} country="in" category="science" />}>  </Route>
+            <Route exact path="/health" element={<News setProgress={this.setProgress} key="health" pageSize={6} country="in" category="health" />}> </Route>
+            <Route exact path="/science" element={ <News setProgress={this.setProgress} key="science" pageSize={6} country="in" category="science" />}>  </Route>
              
           
-            <Route exact path="/sports"  element={<News key="sports" pageSize={6} country="in" category="sports" />}></Route>
+            <Route exact path="/sports"  element={<News setProgress={this.setProgress} key="sports" pageSize={6} country="in" category="sports" />}></Route>
         
-            <Route exact path="/technology" element={<News key="technology" pageSize={6} country="in" category="technology" />}> </Route>
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} key="technology" pageSize={6} country="in" category="technology" />}> </Route>
               
            
           </Routes>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,24 +1,51 @@
 // rce
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-
   render() {
     // syntx of props - destructuring
-    let {title, description,imageUrl,newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
-      <div className='my-3'>
-        <div className="card" >
-            <img src={!imageUrl?"https://earthsky.org/upl/2023/01/Venus-Saturn-jan21-2023-John-Ashley-Sonoita-AZ-e1674391826539.jpg":imageUrl} className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{title}...</h5>
-                <p className="card-text">{description}...</p>
-                <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-            </div>
-           </div>
+      <div className="my-3">
+        <div className="card">
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger "
+            style={{ left: "87%", zIndex: "1" }}
+          >
+            {source}
+          </span>
+          <img
+            src={
+              !imageUrl
+                ? "https://earthsky.org/upl/2023/01/Venus-Saturn-jan21-2023-John-Ashley-Sonoita-AZ-e1674391826539.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text text-danger">
+              <small className="text-danger">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
