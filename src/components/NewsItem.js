633efde5c2a598544092d9c5ddceb0753a2d7201
Sneha2622001 <mesnehasingh26@gmail.com>
2023-01-31 13:17:10 +0530

    major changes function

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,14 @@
 // rce
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react"
 
-export class NewsItem extends Component {
-  render() {
+//converting class base to function base component...................
+// export class NewsItem extends Component {
+  const NewsItem = (props)=>  {
+  // render() {
     // syntx of props - destructuring
     let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
+      props;
     return (
       <div className="my-3">
         <div className="card">
@@ -45,7 +48,7 @@ export class NewsItem extends Component {
         </div>
       </div>
     );
-  }
+  // }
 }
 
 export default NewsItem;
